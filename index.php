07cf5ecc63bcc9edb4b6debf78395e5b9bbeadd1
Taulut Hossain Washi <taulut.hossain@amiprobashi.com>
2024-04-29 10:19:50 +0600

    DEV: implementting refactoring

--- index.php
+++ index.php
@@ -1,99 +1,29 @@
 <?php
 
-interface StrategyInterface
-{
-    public function showTitle(): string;
-}
-
-class StrategyCaps implements StrategyInterface
-{
-
-    /**
-     * @return string
-     */
-    public function showTitle(): string
+class ExtractMethod {
+    public $name = "washi";
+    public function printOwing(): void
     {
-        // TODO: Implement showTitle() method.
+        $this->printBanner();
+        print ("name: ". $this->name);
+        print ("amount: ".$this->getOutstanding());
+        echo "\n";
     }
-}
 
-class StrategyExclaim implements StrategyInterface
-{
+    public function printBanner(): void{
+        echo "This is print banner";
+    }
 
-    /**
-     * @return string
-     */
-    public function showTitle(): string
+    public function getOutstanding(): int
     {
-        // TODO: Implement showTitle() method.
+        return 500;
     }
-}
 
-class StrategyStars implements StrategyInterface{
-
-    /**
-     * @return string
-     */
-    public function showTitle(): string
+    public function printDetails($outstanding): void
     {
-        // TODO: Implement showTitle() method.
+
     }
 }
 
-class StrategyContext {
-    
-    private $strategy = NULL;
-
-    public function __construct($strategy) {
-        switch ($strategy) {
-            case "C":
-                $this->strategy = new StrategyCaps();
-                break;
-            case "E":
-                $this->strategy = new StrategyExclaim();
-                break;
-            case "S":
-                $this->strategy = new StrategyStars();
-                break;
-        }
-    }
-
-
-    public function showBookTitle($book): string
-    {
-        return $this->strategy->showTitle($book);
-    }
-}
-
-class Book
-{
-    private $author;
-    private $title;
-    function __construct($title,$author) {
-        $this->author = $author;
-        $this->title = $title;
-    }
-    public function getAuthor(): string{
-        return $this->author;
-    }
-
-    public function getTitle(): string{
-        return $this->title;
-    }
-    public function getAuthorAndTitle(): string {
-        return $this->getTitle() . " by " . $this->getAuthor();
-    }
-}
-
-writeln('Begin testing strategy pattern');
-writeln('');
-
-$book = new Book('PHP for Cats', 'Larry Truett');
-$strategyContextC= new StrategyContext('C');
-$strategyContextE= new StrategyContext('E');
-$strategyContextS= new StrategyContext('S');
-
-writeln('test 1 - show name context C');
-writeln($strategyContextC->showBookTitle($book));
-writeln('');
-
+$extract = new ExtractMethod();
+$extract->printOwing();
